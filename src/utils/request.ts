2faa554f984bdc69a9ce39193af04471b1aea20c
lorem <1021448137@qq.com>
2023-04-24 15:19:49 +0800

    fix: login bug

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -14,8 +14,10 @@ request.interceptors.request.use(config => {
 
 request.interceptors.response.use(res => res, (err: AxiosError) => {
   // 服务端响应非 2XX。
-  console.log(err);
-  message.error("服务端响应有误，请查看控制台输出");
+  if (err.response!.status === 400) message.error("密码错误");
+  else if (err.response!.status === 401) message.error("用户名不存在");
+  else if (err.response!.status === 403) message.error("无访问权限");
+  else message.error("服务端响应有误，请查看控制台输出");
   return Promise.reject();
 });
 
